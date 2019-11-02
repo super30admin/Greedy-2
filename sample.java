5308ff546e3c929c0067bcde75d2be2a6acb5098
RamyaRaviprakash <rramya@thoughtworks.com>
2019-11-01 20:53:35 -0700

    Update sample.java

--- sample.java
+++ sample.java
@@ -24,7 +24,7 @@ class Solution {
     }
 }
 
-// Time Complexity : O(nlogn)
+// Time Complexity : O(n2)
 // Space Complexity : O(n)
 // Did this code successfully run on Leetcode : yes
 // Any problem you faced while coding this : no
