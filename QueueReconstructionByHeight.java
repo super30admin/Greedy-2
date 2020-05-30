230f4ff9a7c79c51f25eef18b07229fd9609c148
RutujaKaushike <rutujakaushike@gmail.com>
2020-05-29 19:37:54 -0500

    Adding code for both problems

---
+++ QueueReconstructionByHeight.java
@@ -0,0 +1,27 @@
+// Time Complexity : O(n^2)
+// Space Complexity : O(n)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class QueueReconstructionByHeight {
+    public int[][] reconstructQueue(int[][] people) {
+        Arrays.sort(people, (a, b) -> {
+            if(a[0]==b[0]){
+                return a[1] - b[1];
+            } else{
+                return b[0] - a[0];
+            }
+        });
+
+        List<int[]> result = new ArrayList<>();
+        for(int[] person : people){
+            result.add(person[1], person);
+        }
+
+        return result.toArray(new int[0][]);
+    }
+}
