c5c7b1e9af0fc7624fdce4dfcd19a1b1a187c4ec
nileshph <nileshpharate96@gmail.com>
2020-08-23 17:43:03 -0700

    updated time complexity

--- src/QueueReconstruction.java
+++ src/QueueReconstruction.java
@@ -1,3 +1,7 @@
+// Time Complexity : O(n^2)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this :
 
 /**
  * https://leetcode.com/problems/queue-reconstruction-by-height/
