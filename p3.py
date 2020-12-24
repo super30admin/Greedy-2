9ef5364061cf278173667d18ea1bc7bf8bc014ca
Amrutha Prakash Veena <pvamrutha2509@gmail.com>
2020-12-24 10:09:31 -0500

    Create p3.py

---
+++ p3.py
@@ -0,0 +1,24 @@
+#Time: O(N)
+#Space: O(N)
+class Solution:
+    def partitionLabels(self, S: str) -> List[int]:
+        
+        res=[]
+        
+        if S is None or len(S)==0:
+            return res
+        
+        last = {c:i for i,c in enumerate(S)}
+        
+        maxi=0; size =1
+        for i,c in enumerate(S):
+            maxi = max(maxi,last[c])
+            
+            if i==maxi:
+                res.append(size)
+                size=1
+            else:
+                size+=1
+        
+        return res
+            
