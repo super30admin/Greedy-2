015d6d337f544bfc8cdc705f4b1ba587889d118f
pkariya1 <pkariya1@binghamton.edu>
2020-12-25 18:07:07 +0530

    Partition labels completed

--- sample.java
+++ sample.java
@@ -1,7 +1,10 @@
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
-// Any problem you faced while coding this :
+// Time Complexity : O(2*n)
+// Space Complexity : O(1) bcoz we are storing alphabets in HAshmap
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this :No
 
 
 // Your code here along with comments explaining your approach
+//First store all the characters in HashMAp then we will get to know the last index of each character
+//take start and end pointer then 
+//based on window update the start and end pointer.
