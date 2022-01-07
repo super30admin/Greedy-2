b5f5f4cc77cbe69709c1e171376f628bb0de4f8b
Adhish Shrivastava <adhish70@gmail.com>
2022-01-07 12:21:41 -0500

    Problem 141 done

--- Problem-141 Partition Labels.py
+++ Problem-141 Partition Labels.py
@@ -1,11 +1,29 @@
 # 763. Partition Labels
 # https://leetcode.com/problems/partition-labels/
 
-# Logic: 
+# Logic: We make a hashmap with letter as the key and tha last index as value. 
+# We then Iterate over the input and set end variable to the index from hashmap 
+# of the current letter. If the cur index equals the end variable we add to answer.
 
-# Time Complexity: 
-# Space Complexity:
+# Time Complexity: O(n)
+# Space Complexity: O(1) [only 26 letters possible so O(26)]
 
+from collections import defaultdict
 class Solution:
     def partitionLabels(self, s: str) -> List[int]:
-        pass
+        hashmap = defaultdict(int)
+        res = []
+        end = 0
+        start = 0
+        
+        for idx, i in enumerate(s):
+            hashmap[i] = idx
+        
+        for idx, i in enumerate(s):
+            end = max(end, hashmap[i])
+            
+            if idx == end:
+                res.append(end - start + 1)
+                start = end + 1
+        
+        return res
