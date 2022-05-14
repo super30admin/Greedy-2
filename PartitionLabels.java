6f65932de542c6408e456791f6b0c87026fa7fc4
Jeel Patel <JeelPatel@Jeels-MacPro.attlocal.net>
2022-05-14 13:09:42 -0500

    Partition Labels

---
+++ PartitionLabels.java
@@ -0,0 +1,29 @@
+//TC : O(N)
+//SC : O(1) 
+class Solution {
+    public List<Integer> partitionLabels(String s) {
+        List<Integer> res = new ArrayList<>();
+        
+        if(s == null || s.length() == 0)  return res;
+        HashMap<Character, Integer> map = new HashMap<>();
+        
+        for(int i = 0; i< s.length(); i++){
+            char ch = s.charAt(i);
+            map.put(ch, i);
+        }
+        
+        int start = 0, end = 0;
+        
+        for(int i = 0; i< s.length(); i++){
+            char ch = s.charAt(i);
+            end = Math.max(end, map.get(ch));
+            if(i == end){
+                res.add(end - start + 1);
+                start = i + 1; 
+            }
+        }
+        
+        
+        return res;
+    }
+}
