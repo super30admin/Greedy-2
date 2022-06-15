c2b1b7c776c6b21d8aaef1fa73577732fb9d4bdc
nilesh-ghodke <99153455+nilesh-ghodke@users.noreply.github.com>
2022-06-14 23:29:40 -0700

    Add files via upload

---
+++ ReconstuctionByHeight.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Array1
+{
+    class ReconstuctionByHeight
+    {
+        /*
+         * T.C: O(n2) because inserting the array in the list on perticular index cause other elements to move from its original poistion
+         * S.C: O(1)
+         */
+        public int[][] ReconstructQueue(int[][] people)
+        {
+            List<int[]> result = new List<int[]>();
+            Array.Sort(people, (x, y) => {
+                if (x[0] == y[0])
+                    return x[1] - y[1];
+                else
+                    return y[0] - x[0];
+            });
+
+            foreach (int[] p in people)
+            {
+                result.Insert(p[1], p);
+            }
+
+            return result.ToArray();
+        }
+    }
+}
---
+++ TaskScheduler.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Array
+{
+    public class TaskScheduler
+    {
+        /*
+         * T.C: O(n) where n is no of tasks
+         * S.C: O(1) as only 26 chars in the map
+         * 
+         */
+        public int LeastInterval(char[] tasks, int n)
+        {
+
+            if (tasks == null || tasks.Length == 0) return 0;
+
+            Dictionary<char, int> map = new Dictionary<char, int>();
+            int maxFreq = 0;
+            int maxCount = 0;
+
+            for (int i = 0; i < tasks.Length; i++)
+            {
+                if (!map.ContainsKey(tasks[i]))
+                {
+                    map.Add(tasks[i], 1);
+                }
+                else
+                {
+                    map[tasks[i]]++;
+                    maxFreq = Math.Max(maxFreq, map[tasks[i]]);
+                }
+            }
+
+            foreach (char key in map.Keys)
+            {
+                if (map[key] == maxFreq)
+                {
+                    maxCount++;
+                }
+            }
+
+            int Patitions = maxFreq - 1;
+            int emptySlot = Patitions * (n - (maxCount - 1));
+            int PendTasks = tasks.Length - (maxFreq * maxCount);
+            int idleSlot = Math.Max(0, emptySlot - PendTasks);
+
+            return tasks.Length + idleSlot;
+
+        }
+    }
+}
