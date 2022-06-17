96b95ec51951a0d445d631c8d64938eb79825545
Rujal <rujal8452@gmail.com>
2022-06-17 08:57:59 -0400

    Problem2

---
+++ Problem2.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Problem2 {
+    // TC : O(nlogn)
+    // SC : O(n)
+    public int[][] reconstructQueue(int[][] people) {
+        if (people == null) return new int[][]{};
+
+
+        Arrays.sort(people, (a, b) -> {
+            if (a[0] == b[0]) {
+                return a[1] - b[1];
+            }
+            return b[0] - a[0];
+        });
+
+        List<int[]> result = new ArrayList<>();
+
+        for (int[] person : people) {
+            result.add(person[1], person);
+        }
+        return result.toArray(new int[result.size()][]);
+        //return result.toArray(people);
+    }
+}
