ed007f6d5688a40d75dd9a36f5e0c94400c00b3c
Prasad Gagarani <52399176+prasadgagarani@users.noreply.github.com>
2022-08-05 11:19:43 -0400

    Update sample.java

--- sample.java
+++ sample.java
@@ -35,4 +35,46 @@ class Solution {
     }
 }
 
+
+//*****621. TASK SCHEDULER****
+// Time Complexity :o(n);
+// Space Complexity :o(n); for map that we are maintaining
+// Did this code successfully run on Leetcode :Y;
+// Any problem you faced while coding this :N;
+
+class Solution {
+    public int leastInterval(char[] tasks, int n) {
+        HashMap<Character, Integer> map=new HashMap<>();
+        int maxfreq=0;
+        int maxcount=0;
+        
+        //Getting the maxfreq and putting everything into the map;
+        for(char task: tasks)
+        {
+            //Getting the count
+            int count= map.getOrDefault(task, 0);
+            count++;
+            maxfreq=Math.max(maxfreq, count);
+            map.put(task, map.getOrDefault(task,0)+1);
+        }
+        
+        //getting the count of elements that have max freq
+        for(char key: map.keySet())
+        {
+            if(map.get(key)==maxfreq)
+            {
+                maxcount++;
+            }
+        }
+        
+        //Applying all formulas
+        int partition =maxfreq-1;
+        int avaiable= partition*(n-(maxcount-1));
+        int pending=tasks.length-(maxfreq*maxcount);
+        int idle=Math.max(0,avaiable-pending);
+        
+        return tasks.length+idle;
+    }
+}
+
 // Your code here along with comments explaining your approach
