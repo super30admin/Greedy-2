177f767ab188d96c516616bd15b3cec367cd8f06
Prasad Gagarani <52399176+prasadgagarani@users.noreply.github.com>
2022-08-05 09:37:15 -0400

    Update sample.java

--- sample.java
+++ sample.java
@@ -1,7 +1,38 @@
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
-// Any problem you faced while coding this :
+//141.763.Partition Labels
+// Time Complexity :o(n);
+// Space Complexity :o(n); for map that we are maintaining
+// Did this code successfully run on Leetcode :Y;
+// Any problem you faced while coding this :N;
 
+class Solution {
+    public List<Integer> partitionLabels(String s) {
+        HashMap<Character, Integer> map =new HashMap<>();
+        ArrayList<Integer> res=new ArrayList<>();
+        
+        for(int i=0;i<s.length();i++)
+        {
+            char c=s.charAt(i);
+            
+            map.put(c,i);
+        }
+        int start=0;
+        int end=0;
+        for(int i=0;i<s.length();i++)
+        {
+            char c=s.charAt(i);
+            int endofc=map.get(c);
+            end=Math.max(end, endofc);
+            
+            if(i==end)
+            {
+                int cou=end-start+1;
+                res.add(cou);
+                start=i+1;
+            }
+            
+        }
+        return res;
+    }
+}
 
 // Your code here along with comments explaining your approach
