d532162f218f596e8182f6f57299b8551ebcaee7
tathyapatel <92098849+tathyapatel@users.noreply.github.com>
2022-08-08 20:05:45 -0500

    Create PartitionLabels.py

---
+++ PartitionLabels.py
@@ -0,0 +1,26 @@
+#763. Partition Labels
+"""
+Time Complexity : O(n)
+Space Complexity : O(1)
+"""
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+        d = dict()
+        result = []
+        
+        for i in range(0, len(s)):
+            d.update({s[i] : i})
+            
+        #print(d)
+        start = 0
+        end = 0
+        for i in range(0, len(s)):
+            ch = s[i]
+            end = max(end, d.get(ch))
+            
+            if i == end:
+                result.append(end - start + 1)
+                start = i + 1
+        
+        return result
+            
