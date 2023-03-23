9df29858dad89185dd44dfdffc403d2746c2cd81
MalavyaRaval <malavyaraval@gmail.com>
2023-03-23 15:17:23 -0700

    Completed

--- sample.java
+++ sample.java
@@ -1,7 +1,36 @@
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
-// Any problem you faced while coding this :
+// Time Complexity :O(n)
+// Space Complexity :O(n)
+// Did this code successfully run on Leetcode :Yes
+// Any problem you faced while coding this : No
 
 
 // Your code here along with comments explaining your approach
+class Solution {
+    public int leastInterval(char[] tasks, int n) {
+        if(tasks.length ==0)return 0;HashMap<Character, Integer> map = new HashMap<>();
+        int maxCount = 0;
+        int maxFreq = 0;
+        for(int i = 0; i < tasks.length; i++) {
+            if(map.containsKey(tasks[i])) {
+                int count = map.get(tasks[i]) + 1;
+                if(count > maxFreq) {
+                    maxFreq = count;
+                }
+                map.put(tasks[i], count);
+            }
+            else {
+                map.put(tasks[i], 1);
+            }
+        }
+        for(int val : map.values()) {
+            if(val == maxFreq) {
+                maxCount++;
+            }
+        }
+        int partCount = maxFreq - 1;
+        int emptySlots = (n - (maxCount - 1)) * partCount;
+        int availableSlots = tasks.length - (maxCount * maxFreq);
+        int idle = Math.max(0, emptySlots - availableSlots);  
+        return tasks.length + idle;
+    }
+}
