c28d99ea7bd2c40cef330e39faa1251e9a9d2f36
DhyeyaVyas10 <132728547+DhyeyaVyas10@users.noreply.github.com>
2023-08-17 22:13:52 -0400

    Completed Greedy 2

---
+++ prob2.java
@@ -0,0 +1,20 @@
+// Time Complexity : O(n^2)
+// Space Complexity : O(n)
+// Did this code successfully run on Leetcode : Yes
+
+
+class Solution {
+    public int[][] reconstructQueue(int[][] people) {
+        List<int[]> result = new ArrayList<>(); //return value
+        
+        Arrays.sort(people, (a, b) -> {
+            int x = Integer.compare(b[0], a[0]);
+            if(x == 0) return Integer.compare(a[1], b[1]); 
+            else return x; });
+
+        for(int[] p: people)
+            result.add(p[1], p);
+        
+        return result.toArray(new int[people.length][2]);
+    }
+}
