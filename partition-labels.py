67d2f3e3b9c81151a680d73d2ed4f6eed615ba7f
Pavan Kumar Anand <41806225+15apk2000@users.noreply.github.com>
2024-02-29 21:45:47 -0800

    Create partition-labels.py

---
+++ partition-labels.py
@@ -0,0 +1,16 @@
+#Time Complexity : O(N)
+#Space Complexity : O(1)
+
+
+class Solution(object):
+    def partitionLabels(self, S):
+        last = {c: i for i, c in enumerate(S)}
+        j = anchor = 0
+        ans = []
+        for i, c in enumerate(S):
+            j = max(j, last[c])
+            if i == j:
+                ans.append(i - anchor + 1)
+                anchor = i + 1
+            
+        return ans
